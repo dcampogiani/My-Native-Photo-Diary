8b550decebce1f2807097e9729782a8022c8f75b
Daniele Campogiani <daniele.campogiani@gmail.com>
2014-05-23 22:00:19 +0200

    implemented delete

--- MyNativePhotoDiary/src/main/java/com/danielecampogiani/mynativephotodiary/adapters/TimelinePicturesAdapter.java
+++ MyNativePhotoDiary/src/main/java/com/danielecampogiani/mynativephotodiary/adapters/TimelinePicturesAdapter.java
@@ -1,8 +1,13 @@
 package com.danielecampogiani.mynativephotodiary.adapters;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +19,17 @@ import android.widget.TextView;
 import com.danielecampogiani.mynativephotodiary.R;
 import com.danielecampogiani.mynativephotodiary.persistence.PicturesProvider;
 
+import java.io.File;
+
 /**
  * Created by danielecampogiani on 21/05/14.
  */
 public class TimelinePicturesAdapter extends CursorAdapter {
 
     private LayoutInflater myInflater;
+    private static final int ID_TAG = 1;
+    private static final int DESCRIPTION_TAG = 2;
+    private static final int URI_TAG = 3;
 
     public TimelinePicturesAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -32,12 +42,63 @@ public class TimelinePicturesAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(final View view, final Context context, Cursor cursor) {
         ImageView imageView = (ImageView)view.findViewById(R.id.timeline_imageView);
-        TextView description = (TextView)view.findViewById(R.id.timeline_picture_description);
+        TextView descriptionView = (TextView)view.findViewById(R.id.timeline_picture_description);
         Button deleteButton = (Button)view.findViewById(R.id.delete_button);
         Button shareButton = (Button)view.findViewById(R.id.share_button);
-        description.setText(cursor.getString(cursor.getColumnIndex(PicturesProvider.KEY_DESCRIPTION)));
+
+        final int pictureId = cursor.getInt(cursor.getColumnIndex(PicturesProvider.KEY_ID));
+        final String description = cursor.getString(cursor.getColumnIndex(PicturesProvider.KEY_DESCRIPTION));
+        final String uri = cursor.getString(cursor.getColumnIndex(PicturesProvider.KEY_URI));
+
+        //deleteButton.setTag(ID_TAG,id);
+        //deleteButton.setTag(DESCRIPTION_TAG,description);
+        //deleteButton.setTag(URI_TAG,uri);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TimelinePicturesAdapter", "User wanna delete "+pictureId+" " + description+" " + uri);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set title
+                alertDialogBuilder.setTitle("Delete");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Are you sure you want to delete "+ description+ " ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, close
+                                // current activity
+                                ContentResolver cr = context.getContentResolver();
+                                Uri toDelete = ContentUris.withAppendedId(PicturesProvider.CONTENT_URI, pictureId);
+                                cr.delete(toDelete,null,null);
+                                File file = new File(uri);
+                                file.delete();
+
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }
+
+        });
+
+        descriptionView.setText(cursor.getString(cursor.getColumnIndex(PicturesProvider.KEY_DESCRIPTION)));
         imageView.setImageURI(Uri.parse(cursor.getString(cursor.getColumnIndex(PicturesProvider.KEY_URI))));
     }
 }

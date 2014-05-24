5351c27c4b851ad0269139c61de60c3fdc2b2791
Daniele Campogiani <daniele.campogiani@gmail.com>
2014-05-24 15:35:02 +0200

    added map fragment

--- MyNativePhotoDiary/src/main/java/com/danielecampogiani/mynativephotodiary/fragments/PlacesFragment.java
+++ MyNativePhotoDiary/src/main/java/com/danielecampogiani/mynativephotodiary/fragments/PlacesFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.danielecampogiani.mynativephotodiary.R;
+import com.google.android.gms.maps.MapFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +21,11 @@ public class PlacesFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,5 +34,12 @@ public class PlacesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_places, container, false);
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        MapFragment f = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        if (f != null)
+            getFragmentManager().beginTransaction().remove(f).commit();
+    }
 
 }
